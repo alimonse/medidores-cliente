d82c334cc1601322c36ab6809f6ed0b9a58a4395
Alimonse <alimonse29@gmail.com>
2022-07-01 23:37:58 -0500

    token

--- medidores-cliente/src/modulos/servicios-externos/api-medidores.service.ts
+++ medidores-cliente/src/modulos/servicios-externos/api-medidores.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import {HttpService} from "@nestjs/axios";
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class ApiMedidoresService {
-    constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) {}
 
-    async authAPI(){
-       return this.httpService.get('http://172.17.46.137:8080/UIP/uip/uipManage/dataOper?interfaceCode=Authorization&userName=JRAMI &password=Jr@123456')
-           .subscribe(resp => {
-               console.log(resp);})
+  async authAPI() {
+    const json = await firstValueFrom(
+      this.httpService
+        .get(
+          'http://172.17.46.137:8080/UIP/uip/uipManage/dataOper?interfaceCode=Authorization&userName=JRAMI &password=Jr@123456',
+        )
+        .pipe(
+          map((rta) => {
+            return rta.status === 200 ? rta.data : null;
+          }),
+        ),
+    );
+    if ('data' in json && 'token_id' in json.data) {
+      return json.data.token_id;
     }
+    return null;
+  }
 }
--- medidores-cliente/src/modulos/usuario/usuario.service.ts
+++ medidores-cliente/src/modulos/usuario/usuario.service.ts
@@ -1,40 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import {InjectModel} from "@nestjs/mongoose";
-import {Model} from "mongoose";
-import {Usuario} from "./usuario.schema";
-import {CreateUserDto, UsuarioPassDto} from "./dto/usuario.create.dto";
-import {ServiciosExternosModule} from "../servicios-externos/servicios-externos.module";
-import {ApiMedidoresService} from "../servicios-externos/api-medidores.service";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Usuario } from './usuario.schema';
+import { CreateUserDto, UsuarioPassDto } from './dto/usuario.create.dto';
+import { ServiciosExternosModule } from '../servicios-externos/servicios-externos.module';
+import { ApiMedidoresService } from '../servicios-externos/api-medidores.service';
 
 @Injectable()
 export class UsuarioService {
+  constructor(
+    @InjectModel(Usuario.name)
+    private readonly _usuarioModel: Model<Usuario>,
+    private readonly _serviciosExternos: ApiMedidoresService,
+  ) {}
 
-    constructor(
-        @InjectModel(Usuario.name)
-        private readonly _usuarioModel: Model<Usuario>,
-        private readonly _serviciosExternos: ApiMedidoresService
-    ) {
+  crear(datosUsuario: CreateUserDto) {
+    const nuevoUsuario = new this._usuarioModel(datosUsuario);
+    return nuevoUsuario.save();
+  }
 
+  obtener() {
+    return this._usuarioModel.find().exec();
+  }
+
+  async buscarPorUsuarioyContrasena(criterioBusqueda: UsuarioPassDto) {
+    try {
+      console.log(await this._serviciosExternos.authAPI());
+      this._serviciosExternos.authAPI();
+      const { cedula, password } = criterioBusqueda;
+      return await this._usuarioModel.findOne({ cedula, password }).exec();
+    } catch (e) {
+      return 0;
     }
-
-    crear(datosUsuario: CreateUserDto){
-        const nuevoUsuario = new this._usuarioModel(datosUsuario);
-        return nuevoUsuario.save();
-    }
-
-    obtener(){
-        return this._usuarioModel.find().exec();
-    }
-
-    async buscarPorUsuarioyContrasena(criterioBusqueda: UsuarioPassDto){
-        try {
-            this._serviciosExternos.authAPI();
-            const {cedula,password} = criterioBusqueda;
-            return await this._usuarioModel.findOne({cedula,password}).exec()
-        } catch (e) {
-            return 0;
-        }
-    }
-
+  }
 }
-
